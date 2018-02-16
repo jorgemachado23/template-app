78cd07685b1a98ac894a284cfe1e5ba948ed9619
ACUL\jmachado <jorge.machado@atrium-uw.com>
2018-02-16 10:36:48 +0000

    Fix test using the proper default nagivation

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-import {AppComponent} from '../../app.component';
 import {GridModule} from '@progress/kendo-angular-grid';
 
 describe('HomeComponent', () => {
