20667f304a555a23cd974005d8da038fa89d4ecb
ACUL\jmachado <jorge.machado@atrium-uw.com>
2018-02-19 10:04:46 +0000

    Remove loader directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import {GridModule} from '@progress/kendo-angular-grid';
 import {HomeComponent} from './components/home/home.component';
@@ -9,7 +8,6 @@ import {AppRoutingModule} from '../app-routing.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import {ProductService} from './services/product.service';
 import {FormsModule} from '@angular/forms';
-import {LoaderDirective} from './directives/loader.directive';
 import {DateInputsModule} from '@progress/kendo-angular-dateinputs';
 import {DropDownsModule} from '@progress/kendo-angular-dropdowns';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -24,8 +22,7 @@ import {MatProgressBarModule} from '@angular/material';
   declarations: [
     AppComponent,
     HomeComponent,
-    SidebarComponent,
-    LoaderDirective,
+    SidebarComponent
   ],
   imports: [
     MatProgressBarModule,
