078bf289bcba73ccc96539e5cbc97752b5038311
ACUL\jmachado <jorge.machado@atrium-uw.com>
2018-02-19 10:48:43 +0000

    Add integrations tests examples

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -23,4 +23,15 @@ describe('template-app App', () => {
     const rows = element.all(by.css('kendo-grid tbody>tr'));
     expect(rows.count()).toBe(77);
   });
+
+  it('should contain a dialog when open modal is clicked', () => {
+    page.navigateTo();
+    const btnModal = element(by.css('#btn-modal'));
+    expect(btnModal).toBeTruthy();
+    expect(btnModal.getText()).toBe('Open modal');
+    btnModal.click().then((x) => {
+      const dialog = element(by.css('kendo-dialog'));
+      expect(dialog).not.toBeNull();
+    });
+  });
 });
