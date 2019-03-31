9bf644922ad31e9ac2e2f5dcc0c8f7dbcef7d061
Jorge Machado Torres <>
2019-03-31 18:31:31 +0100

    add stuff

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as auth0 from 'auth0-js';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +11,13 @@ export class AuthService {
   private _accessToken: string;
   private _expiresAt: number;
   userProfile: any;
-
-
-  auth0 = new auth0.WebAuth({
-    clientID: '7snPSL15XykPMedjCMwYR2MSDJQy2n7Y',
-    domain: 'jorge-testing.eu.auth0.com',
-    responseType: 'token id_token',
-    redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid profile'
-  });
+  auth0;
 
   constructor(public router: Router) {
     this._idToken = '';
     this._accessToken = '';
     this._expiresAt = 0;
+    this.initializeAuth0();
   }
 
   get accessToken(): string {
@@ -54,11 +47,23 @@ export class AuthService {
 
   }
 
+  private initializeAuth0(): void {
+    this.auth0 = new auth0.WebAuth({
+      clientID: '7snPSL15XykPMedjCMwYR2MSDJQy2n7Y',
+      domain: 'jorge-testing.eu.auth0.com',
+      responseType: 'token id_token',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile'
+    });
+  }
+
   private localLogin(authResult): void {
     // Set isLoggedIn flag in localStorage
     localStorage.setItem('isLoggedIn', 'true');
     // Set the time that the access token will expire at
-    const expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    const currentDate = new Date();
+    currentDate.setMinutes(currentDate.getMinutes() + 2);
+    const expiresAt = currentDate.getTime();
     this._accessToken = authResult.accessToken;
     this._idToken = authResult.idToken;
     this._expiresAt = expiresAt;
@@ -76,13 +81,10 @@ export class AuthService {
   }
 
   public logout(): void {
-    // Remove tokens and expiry time
     this._accessToken = '';
     this._idToken = '';
     this._expiresAt = 0;
-    // Remove isLoggedIn flag from localStorage
     localStorage.removeItem('isLoggedIn');
-    // Go back to the home route
     this.router.navigate(['/']);
   }
 
@@ -90,19 +92,6 @@ export class AuthService {
     // Check whether the current time is past the
     // access token's expiry time
     return new Date().getTime() < this._expiresAt;
-  }
-
-  public getUserInfo() {
-
-    if (!this._accessToken) {
-      throw new Error('Access token must exist to fetch profile');
-    }
-
-    this.auth0.client.userInfo(this._accessToken, (err, profile) => {
-      if (profile) {
-        this.userProfile = profile;
-      }
-    });  
   }
 
 }
